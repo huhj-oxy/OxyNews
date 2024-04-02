7e9b2bd0348ed951e27daa85c4195da2d1d7ccda
huhj-oxy <huhj@oxy.edu>
2024-04-01 19:02:43 -0700

    commit (James)

---
+++ app/src/main/java/com/example/oxynews/ArticleData.java
@@ -0,0 +1,11 @@
+package com.example.oxynews;
+
+import java.util.ArrayList;
+
+public class ArticleData {
+    private String articleID;
+    private String title;
+    private String author;
+    private Integer date;
+    private ArrayList<String> tags;
+}
--- app/src/main/java/com/example/oxynews/MainActivity.java
+++ app/src/main/java/com/example/oxynews/MainActivity.java
@@ -2,7 +2,20 @@ package com.example.oxynews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +23,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // String text = FileUtils.readFromFile(this, "oxyNewsData.txt");
+        // Log.i("XXX", text);
     }
 }
+
+/*
+class FileUtils {
+    public static String readFromFile(Context context, String filename) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            InputStream inputStream = context.getAssets().open(filename);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line).append("\n");
+            }
+            bufferedReader.close();
+            inputStream.close();
+        } catch (IOException e) {
+            Toast.makeText(context, "Cannot Read File", Toast.LENGTH_SHORT).show();
+        }
+        return stringBuilder.toString();
+    }
+}*/
